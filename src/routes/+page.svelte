32341f698338f0c49f04b90683d5137c7b75e174
Sidney Lemon <sid.lemon3@gmail.com>
2023-04-05 19:50:50 -0600

    initial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  export let data;
+  let { billionaire } = data;
+
+  console.log(data);
+</script>
+
+<h1>Billionaires</h1>
+<ul>
+  {#each billionaire as billionaire}
+    <ul>
+      {billionaire.id}:
+      {billionaire.name},
+      {billionaire.age},
+      {billionaire.country_name},
+      {billionaire.industry_name}
+    </ul>
+  {/each}
+</ul>
