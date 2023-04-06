418f3aa8825414788137ce56f55118c6aac6bedf
rashid <abdulrashid.hayato@edu.sait.ca>
2023-04-06 10:40:57 -0600

    added list

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,5 +1,5 @@
 <nav>
-	<a href="/">Home</a>
-	<a href="/billionaires">Billionaires</a>
-	<a href="/chart">Chart</a>
+	 <li> <a href="/">Home</a> </li>
+	 <li> <a href="/billionaires">Billionaires</a> </li>
+	 <li> <a href="/chart">Chart</a>  </li>
 </nav>
