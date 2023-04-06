06f90c0f96e6080d32ac7212fbd267377b3982c7
Kaleb-cpu <kakuj424@gmail.com>
2023-04-06 14:54:19 -0600

    id added

--- src/routes/billionaires/[id]/+page.server.js
+++ src/routes/billionaires/[id]/+page.server.js
@@ -2,20 +2,28 @@ import { supabase } from "$lib/supabaseClient";
 
 export async function load({params}) {
   
-let { data, error } = await supabase.from("billionaire").select(`
+let { data, error } = await supabase
+  .from("billionaire")
+  .select(
+    `
     *,
     country (
       country_name
     ),
     industry (
       industry_name
+    ),
+    billionaire_info (
+      source_id (
+        source
+      )
     )
-  `)
-  .eq('id', params.id)
+  `
+  )
+  .eq("id", params.id);
       console.log(error)
   return {
     billionaire: data ?? [],
-  
   };
 }
           
--- src/routes/billionaires/[id]/+page.svelte
+++ src/routes/billionaires/[id]/+page.svelte
@@ -3,10 +3,9 @@
   console.log(data)
 </script>
 
-<h1>Hello World</h1>
 
 {#if data.billionaire[0].id}
-<h1>Hello {data.billionaire[0].name}</h1>
+<h1>{data.billionaire[0].name}, {data.billionaire[0].age} years of age and is one of the richest people in the world and he is from {data.billionaire[0].country.country_name}. Their main field is {data.billionaire[0].industry.industry_name} and he works in {data.billionaire[0].billionaire_info[0].source_id.source}. </h1>
 {:else}
 <h1>Billionaire not found</h1>
 {/if}
