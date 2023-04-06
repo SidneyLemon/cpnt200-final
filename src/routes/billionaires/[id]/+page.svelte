dff9e1cc8bd8ea38abf9b636d34fbcd5b2c5af69
Kaleb-cpu <kakuj424@gmail.com>
2023-04-06 15:04:09 -0600

    typo fixed

--- src/routes/billionaires/[id]/+page.svelte
+++ src/routes/billionaires/[id]/+page.svelte
@@ -5,7 +5,7 @@
 
 
 {#if data.billionaire[0].id}
-<h1>{data.billionaire[0].name}, {data.billionaire[0].age} years of age and is one of the richest people in the world and he is from {data.billionaire[0].country.country_name}. Their main field is {data.billionaire[0].industry.industry_name} and he works in {data.billionaire[0].billionaire_info[0].source_id.source}. </h1>
+<h1>{data.billionaire[0].name}, {data.billionaire[0].age} years of age and is one of the richest people in the world and he is from {data.billionaire[0].country.country_name}. Their main field is {data.billionaire[0].industry.industry_name} and they work in {data.billionaire[0].billionaire_info[0].source_id.source}.</h1>
 {:else}
 <h1>Billionaire not found</h1>
 {/if}
